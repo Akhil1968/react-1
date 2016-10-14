1b2aac2f9c0352891031907d1228197aa63840c5
Akhilanand Singh <akhilanandsingh@gmail.com>
2016-10-14 20:33:34 +0530

    reorg code

--- public/2.props.2/myjsx.jsx
+++ public/2.props.2/myjsx.jsx
@@ -1,37 +1,36 @@
-var ComponentOne = React.createClass({
-  render: function() {
+class ComponentOne extends React.Component{
+  render() {
     return (
-      <div>
-        <h1> This is component one </h1>
+      <div className="well">
+        <h3> This is component one </h3>
         <ComponentTwo
           propString="this is string from ComponentOne"
-          propObject={ { name: 'Tomy', type:"dog"} }
+          propObject={ { name: 'Bravo', type:"DOG"} }
           propArray={ [ { name: 'Tom', type:"Cat"},
                         { name: 'Jerry', type:"Mouse"},
-                        { name: 'Butch', type:"Dog"}
+                        { name: 'Donald', type:"Duck"}
                       ] }
           />
       </div>
     );
   }
-});
+}
 
-var ComponentTwo = React.createClass({
-  render: function() {
+class ComponentTwo extends React.Component{
+  render() {
     return (
-      <div>
+      <div className="well">
         <h3> this is component two </h3>
         <p>{this.props.propString}</p>
-        <p>{this.props.propObject.name}
-          &nbsp; {this.props.propObject.type}</p>
-        <p>{this.props.propArray[0].name}
-          &nbsp; {this.props.propArray[1].name}
-          &nbsp; {this.props.propArray[2].name}
-        </p>
+        <p>{this.props.propObject.name} is a {this.props.propObject.type}</p>
+        <p>{this.props.propArray[0].name}&nbsp;{this.props.propArray[0].type}</p>
+        <p>{this.props.propArray[1].name}&nbsp;{this.props.propArray[1].type}</p>
+        <p>{this.props.propArray[2].name}&nbsp;{this.props.propArray[2].type}</p>
+        
       </div>
     )
   }
-});
+};
 
 ReactDOM.render(
   <ComponentOne/>,
